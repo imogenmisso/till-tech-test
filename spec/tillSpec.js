33b5a1a0b6ce70e08e2713b3e641e18db34e3505
Imogen Misso <imogenmisso@hotmail.co.uk>
2019-01-10 14:11:04 +0000

    New features added - can calculate discount for a given order - only applies discount to orders over 50 - displays total in correct format

--- spec/tillSpec.js
+++ spec/tillSpec.js
@@ -28,7 +28,7 @@ describe("Till", function() {
   it("can return the total of the order", function() {
     till.add("Cafe Latte", 4.75, 2)
     till.add("Hot Chocoloate", 4.5, 1)
-    expect(till.displayTotal()).toEqual(14)
+    expect(till.displayTotal()).toEqual('14.00')
   })
 
   it("can calculate the amount of tax for an order", function() {
@@ -41,5 +41,19 @@ describe("Till", function() {
     till.add("Cafe Latte", 4.75, 2)
     till.add("Hot Chocoloate", 4.5, 1)
     expect(till.calculateChange(20)).toEqual(6)
+  })
+
+  it("can calculate the discount for a given order", function() {
+    till.add("Cafe Latte", 4.75, 2)
+    till.add("Hot Chocoloate", 4.5, 1)
+    till.add("Choc Mudcake", 6.4, 10)
+    expect(till.applyDiscount()).toEqual('74.10')
+  })
+
+  it("only applies discount to orders over 50", function() {
+    till.add("Cafe Latte", 4.75, 2)
+    till.add("Hot Chocoloate", 4.5, 1)
+    till.add("Choc Mudcake", 6.4, 4)
+    expect(till.displayTotal()).toEqual('39.60')
   })
 })
--- src/till.js
+++ src/till.js
@@ -1,7 +1,8 @@
 function Till() {
   this.total = 0;
   this.order = [];
-  this.TAX = 8.64
+  this.TAX = 8.64;
+  this.DISCOUNT = 5
 }
 
 Till.prototype.add = function(item, price, quantity) {
@@ -10,7 +11,11 @@ Till.prototype.add = function(item, price, quantity) {
 }
 
 Till.prototype.displayTotal = function() {
-  return this.total;
+  if (this.total >= 50) {
+    this.applyDiscount()
+  } else {
+    return this.total.toFixed(2);
+  }
 }
 
 Till.prototype.calculateTax = function() {
@@ -18,5 +23,10 @@ Till.prototype.calculateTax = function() {
 }
 
 Till.prototype.calculateChange = function(moneyGiven) {
-  return moneyGiven - this.total
+  return moneyGiven - this.total;
+}
+
+Till.prototype.applyDiscount = function() {
+  this.total = (this.total - this.total * (this.DISCOUNT / 100)).toFixed(2)
+  return this.total
 }
